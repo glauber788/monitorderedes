e7a621b123993552ce12cd54060e894ec08d216a
glauber788 <50890688+glauber788@users.noreply.github.com>
2019-05-23 19:29:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head><!-- CABEÇA -->
+<meta charset="utf-8"><!-- AUTORIZANDO SINALIZAÇÃO -->
+<title>Monitor de Rede</title><!-- TITULO -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><!-- LINK DO CSS ONLINE -->
+<link rel="stylesheet" href="css/estilo.css"><!-- ACESSANDO CSS NO DIRETORIO	 -->
+</head>
+<body><!--CORPO-->
+	<style> 
+	p {background-color:white}
+	</style>
+<div class="container">
+<p><h1>Monitor de redes</h1></p>
+<form method="post" class="form-inline">
+<div class="row">
+	<div class="col">
+		<input type="text" placeholder="Nome" name="nomecomputador" class="form-control">
+	</div>
+	<div class="col">
+		<input type="text" placeholder="Ip" name="ip" class="form-control">
+	</div>
+	<div class="col">
+		<input type="submit" name="btn btn-outline-secondary" value="salvar">
+	</div>
+</div>
+</form>
+<div class="row">
+	<div class="col">
+
+</div>
+
+</body>
+</html>
